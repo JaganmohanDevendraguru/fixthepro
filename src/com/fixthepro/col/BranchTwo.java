3bdc514fd0b759858a18c2c7e9cdea6b6279c130
JaganmohanDevendraguru <jegan0307@gmail.com>
2020-05-05 00:07:14 -0400

    added BranchTwo.java for new branch

---
+++ src/com/fixthepro/col/BranchTwo.java
@@ -0,0 +1,21 @@
+/**
+ * jegan
+ * 2020
+ */
+package com.fixthepro.col;
+
+/**
+ * @author jegan
+ *
+ */
+public class BranchTwo {
+
+	/**
+	 * @param
+	 * void
+	 */
+	public static void main(String[] args) {
+		System.out.println("New branch 2");
+	}
+
+}
