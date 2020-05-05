9757c01933372d5a2ef25c86ca38a3de04b7a3cc
JaganmohanDevendraguru <jegan0307@gmail.com>
2020-05-04 21:07:23 -0400

    First commit for this project with a single java file

---
+++ src/com/fixthepro/example/HelloToGit.java
@@ -0,0 +1,21 @@
+/**
+ * jegan
+ * 2020
+ */
+package com.fixthepro.example;
+
+/**
+ * @author jegan
+ *
+ */
+public class HelloToGit {
+
+	/**
+	 * @param
+	 * void
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello Git first program!");
+	}
+
+}
