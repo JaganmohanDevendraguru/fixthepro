f93f3f8139733f2ad8ea5457779819a1b6af2d89
JaganmohanDevendraguru <jegan0307@gmail.com>
2020-05-05 23:35:50 -0400

    exception class added

---
+++ src/com/fixthepro/example/ExceptionTest.java
@@ -0,0 +1,38 @@
+/**
+ * jegan
+ * 2020
+ */
+package com.fixthepro.example;
+
+import java.io.IOException;
+
+/**
+ * @author jegan
+ *
+ */
+public class ExceptionTest {
+
+	/**
+	 * @param
+	 * void
+	 */
+	public static void main(String[] args) {
+		try {
+			exMethod();
+		} catch (IOException e) {
+			System.out.println("Handled in Main");
+			e.printStackTrace();
+		}
+	}
+	
+	static void exMethod() throws IOException{
+		try {
+			int i =0;
+			if(i == 0)
+				throw new Exception("i is zero");
+		} catch (Exception e) {
+			System.out.println("exMethod ex");
+		}
+	}
+
+}
