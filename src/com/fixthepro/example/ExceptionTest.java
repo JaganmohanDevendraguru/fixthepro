762cd4bae35090145d3dcad55e2ea9c9c2c10925
JaganmohanDevendraguru <jegan0307@gmail.com>
2020-05-09 10:51:37 -0400

    m() added to branch from stashing stage

---
+++ src/com/fixthepro/example/ExceptionTest.java
@@ -0,0 +1,43 @@
+/**
+ * jegan
+ * 2020
+ */
+package com.fixthepro.example;
+
+import java.io.IOException;
+
+/**
+ * @author jegan
+ *
+ */
+public class ExceptionTest {
+
+	/**
+	 * @param
+	 * void
+	 */
+	public static void main(String[] args) {
+		try {
+			exMethod();
+		} catch (IOException e) {
+			System.out.println("Handled in Main");
+			e.printStackTrace();
+		}
+	}
+	
+	public void print() {}
+	
+	public void m() {}
+	
+	static void exMethod() throws IOException{
+		try {
+			int i =0;
+			if(i == 0)
+				throw new Exception("i is zero");
+		} catch (Exception e) {
+			System.out.println("exMethod ex");
+			e.printStackTrace();
+		}
+	}
+
+}
